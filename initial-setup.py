fcb900689d2a6ad917c69397d6c6dc15c5f75d79
JeffreyDinackus <jeffreydinackus@proton.me>
2023-07-16 16:11:14 -0400

    bug where it won't let me save to env, changing to files

--- initial-setup.py
+++ initial-setup.py
@@ -1,42 +1,47 @@
-#! /bin/python3
-
 import os
 
-def save_phone_numbers(my_phone, twilio_phone, account_sid, auth_token, assets_classic):
-    # Set phone numbers as environment variables
-    os.environ['MY_PHONE'] = my_phone
-    os.environ['TWILIO_PHONE'] = twilio_phone
-    os.environ['TWILIO_ACCOUNT_SID'] = account_sid
-    os.environ['TWILIO_AUTH_TOKEN'] = auth_token
-    os.environ['ASSETS_CLASSIC_LINK'] = assets_classic
-    
+def save_phone_numbers_to_files(my_phone, twilio_phone, account_sid, auth_token, assets_classic):
+    # Save phone numbers to separate files
+    with open('my_phone.txt', 'w') as file1:
+        file1.write(my_phone)
+        
+    with open('twilio_phone.txt', 'w') as file2:
+        file2.write(twilio_phone)
+        
+    with open('account_sid.txt', 'w') as file3:
+        file3.write(account_sid)
+        
+    with open('auth_token.txt', 'w') as file4:
+        file4.write(auth_token)
+
+    with open('assets_classic_link.txt', 'w') as file5:
+        file5.write(assets_classic)
+
 # Prompt the user for phone numbers
 def main():
     print('Welcome to DateEscape!')
     print('Please enter your info EXACTLY or it will not work')
-    print('This script will save your info to environment variables')
-    print('If you restart your computer, you will have to run this script again')
+    print('This script will save your info to files')
+    print('If you restart your computer, you can load your info from the files')
     print('You can find your Twilio Account SID and Auth Token at https://www.twilio.com/console')
     print('You can find your Twilio phone number at https://www.twilio.com/console/phone-numbers/incoming')
-    print('The developer reccomends copying and pasting in most cases to avoid fustration')
-    print("after running this script once, files will be created for each input, and you can directly edit each one in the respective .txt file")
+    print('The developer recommends copying and pasting in most cases to avoid frustration')
+
     my_phone = input("Enter the phone number you would like to call in this format (USA country code is +1): +1xxxxxxxxxx ")
     twilio_phone = input("Enter your Twilio phone number with the country code in this format (USA country code is +1): +1xxxxxxxxxx ")
     account_sid = input("Enter your Twilio Account Sid: ")
     auth_token = input("Enter your Twilio Auth Token (remember to keep this secret): ")
-    assets_classic = input("Enter your link to assets classic on twilio. This is the link to the mp3 file you want to play. You will need the full link and upload it yourself. (under functions and assets -> Assets(classic), a url) ")
+    assets_classic = input("Enter your link to assets classic on Twilio. This is the link to the mp3 file you want to play. You will need the full link and upload it yourself. (under functions and assets -> Assets(classic), a url) ")
 
-    print("you have entered the following: ")
+    print("\nYou have entered the following:")
     print("Your Phone Number:", my_phone)
     print("Twilio Account SID:", account_sid)
     print("Twilio Auth Token:", auth_token)
     print("Twilio Phone Number:", twilio_phone)
     print("Twilio Assets Classic Link: ", assets_classic)
 
-
-    # Call the function to save phone numbers
-    save_phone_numbers(my_phone, twilio_phone, account_sid, auth_token, assets_classic)
-
+    # Save phone numbers to files
+    save_phone_numbers_to_files(my_phone, twilio_phone, account_sid, auth_token, assets_classic)
 
 if __name__ == "__main__":
     main()
